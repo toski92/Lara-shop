0ac8e701885bb30a0fea9e237b955d6ba8a1b7e7
Toski92 <otochukwu2@gmail.com>
2023-10-24 04:05:44 +0100

    code clean-up

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -2,7 +2,7 @@
     <div class="container mx-auto">
         <div class="bg-white max-w-2xl px-4 py-4 mx-auto sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
             <div>
-                @if (!empty($products))
+                @if ($products->count())
                     <h2 class="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
 
                     <div class="grid grid-cols-1 mt-6 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
