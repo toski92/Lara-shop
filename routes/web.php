66124ce37b1fcdbb97e7ffcb9cb048c9f8154ac7
Toski92 <otochukwu2@gmail.com>
2023-11-06 17:16:21 +0100

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,10 +25,13 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/dashboard/all-users', [UserController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard.users');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::delete('/delete-user/{id}', [UserController::class, 'destroy'])->middleware('checkUserRole')->name('delete-user.destroy');
     Route::get('/product/add-product', [ProductController::class,'create'])->middleware('checkUserRole')->name('add.product');
     Route::post('/product/add-product', [ProductController::class,'store'])->name('store.product');
 });
